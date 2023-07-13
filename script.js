7ea257d7ead585a72108108c83a7efd0bd1b2502
譚丞岡  ChengKang Tan <87925027+TCK2001@users.noreply.github.com>
2023-07-13 23:48:31 +0800

    Update script.js

--- script.js
+++ script.js
@@ -16,20 +16,3 @@ window.addEventListener('DOMContentLoaded', function() {
       accordion.style.display = 'none';
     }
   }
-  function submitChat() {
-    var nicknameInput = document.getElementById("nickname-input");
-    var chatInput = document.getElementById("chat-input");
-    var messageList = document.getElementById("message-list");
-  
-    var nickname = nicknameInput.value;
-    var chat = chatInput.value;
-  
-    if (nickname && chat) {
-      var listItem = document.createElement("li");
-      listItem.innerHTML = "<b>" + nickname + ":</b> " + chat;
-      messageList.appendChild(listItem);
-  
-      nicknameInput.value = "";
-      chatInput.value = "";
-    }
-  }
