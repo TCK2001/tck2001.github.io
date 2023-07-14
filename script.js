1b6e55ab090c496de725d0b08d76cb5e1e70f5af
譚丞岡  ChengKang Tan <87925027+TCK2001@users.noreply.github.com>
2023-07-15 00:45:26 +0800

    Update script.js

--- script.js
+++ script.js
@@ -16,3 +16,34 @@ window.addEventListener('DOMContentLoaded', function() {
       accordion.style.display = 'none';
     }
   }
+var randomProblem;
+
+// 문제 출제
+function generateProblem() {
+  // 이전에 출제된 문제와 중복되지 않도록 문제 목록에서 제거
+  var availableProblems = problems.filter(function(problem) {
+    return problem !== randomProblem;
+  });
+
+  // 문제 목록에서 랜덤하게 선택
+  randomProblem = availableProblems[Math.floor(Math.random() * availableProblems.length)];
+
+  // 문제 출력
+  var problemElement = document.getElementById("problem");
+  problemElement.textContent = randomProblem.question;
+}
+
+// 정답 체크
+function checkAnswer() {
+  var answerInput = document.getElementById("answer-input").value;
+  var resultElement = document.getElementById("result");
+  
+  if (randomProblem.answers.includes(answerInput)) {
+    resultElement.textContent = "정답!";
+  } else {
+    resultElement.textContent = "다시 생각해봐.";
+  }
+  setTimeout(function() {
+    resultElement.textContent = "";
+  }, 2000);
+}
